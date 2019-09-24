8a1f88ccd52aaa0a4842882127142116d4d450c9
Joao Silva <joaosdev@gmail.com>
2019-09-24 12:02:32 +0100

    Added Item and Iventory

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int id;
+    public string title;
+    public string location;
+    public double latitude;
+    public double longitude;
+}
---
+++ Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    public Item GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    public Item GetItem(string name)
+    {
+        return items.Find(item => item.title == name);
+    }
+}
