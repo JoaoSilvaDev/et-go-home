ee7a132c630ee85401ef50803e5d63003d37595e
Joao Silva <joaosdev@gmail.com>
2019-09-24 12:57:53 +0100

    Changed library directories

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    // Range around character where player can grab items
+    public float range;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+            GrabItem();
+    }
+
+    void GrabItem()
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(touch.position);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.gameObject.CompareTag("Item"))
+                {
+                    Debug.Log("Collected " + hit.collider.gameObject.GetComponent<Item>().title);
+                }
+            }
+        }
+    }
+}
