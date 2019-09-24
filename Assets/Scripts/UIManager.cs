3cc11f75cac68198d977c90034a15a81163b9e53
tehdevv <41214812+tehdevv@users.noreply.github.com>
2019-09-24 10:46:31 +0100

    Start and End Screens

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject finishUI;
+    public GameObject givePieceButton;
+
+    public void GivePieceButton()
+    {
+        givePieceButton.SetActive(false);
+        finishUI.SetActive(true);
+    }
+}
