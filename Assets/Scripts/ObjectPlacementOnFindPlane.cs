0eb3bf8373df292502514fd6fd6e51ff7f9ed89b
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-09-26 12:22:01 +0100

    Start Scene 02

---
+++ Assets/Scripts/ObjectPlacementOnFindPlane.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ObjectPlacementOnFindPlane : MonoBehaviour
+{
+
+    [SerializeField] private ARPlaneManager planeManager;
+    [SerializeField] private GameObject objectToInstantiate;
+    [SerializeField] private UnityEvent onSuitablePlaneFound;
+
+    private bool _found;
+
+    private void Start()
+    {
+        planeManager.planesChanged += OnChangePlanes;
+    }
+
+    private void OnDestroy()
+    {
+        if(planeManager)
+            planeManager.planesChanged -= OnChangePlanes;
+    }
+
+    private void OnChangePlanes(ARPlanesChangedEventArgs args)
+    {
+        if (_found)
+            return;
+
+        for (int i = 0; i < args.added.Count; i++)
+        {
+            if (IsPlaneSuitable(args.added[i]))
+            {
+                OnSuitablePlaneFound(args.added[i]);
+                return;
+            }
+        }
+
+        for (int i = 0; i < args.updated.Count; i++)
+        {
+            if (IsPlaneSuitable(args.updated[i]))
+            {
+                OnSuitablePlaneFound(args.updated[i]);
+                return;
+            }
+        }
+    }
+
+    private bool IsPlaneSuitable(ARPlane plane)
+    {
+        var alignment = plane.alignment;
+        if (alignment == PlaneAlignment.HorizontalDown || alignment == PlaneAlignment.HorizontalUp)
+        {
+            return true;
+        }
+
+
+        return false;
+    }
+
+    private void OnSuitablePlaneFound(ARPlane plane)
+    {
+        onSuitablePlaneFound.Invoke();
+        Instantiate(objectToInstantiate, plane.center, Quaternion.identity);
+        _found = true;
+    }
+}
---
+++ Assets/Scripts/Tests Only/SwitchScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.touchCount == 3 && Input.touches[2].phase == TouchPhase.Began)
+        {
+            int index = SceneManager.GetActiveScene().buildIndex +1;
+            index = index % SceneManager.sceneCount;
+            SceneManager.LoadScene(index);
+        }
+    }
+}
