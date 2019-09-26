e989d9d3503295e1bf1569548b0f6cd9f7367d96
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-09-26 17:48:45 +0100

    Scene 1 traNSITION TO 2

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private int sceneIndex = 0;
+    [SerializeField] private bool loadOnEnable = false;
+
+    private void OnEnable()
+    {
+        if (loadOnEnable)
+            Load();
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void Load(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float time;
+    [SerializeField] private UnityEvent onFinishCounter;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(time);
+        onFinishCounter.Invoke();
+    }
+}
