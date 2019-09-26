c5f2ef0a78cdf6c3f119a23017e3f5e34d3f067f
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-09-26 10:33:48 +0100

    testing multiple tecnologies in the same scene :D

---
+++ Assets/Scripts/Tests Only/SwitchPrefabs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPrefabs : MonoBehaviour
+{
+    public GameObject[] objs;
+
+    int index;
+
+    private void Update()
+    {
+        if (objs.Length == 0)
+            return;
+
+        if(Input.touchCount == 3 && Input.touches[2].phase == TouchPhase.Began)
+        {
+
+            for (int i = 0; i < objs.Length; i++)
+            {
+                if (i == index)
+                    objs[i].SetActive(true);
+                else
+                    objs[i].SetActive(false);
+            }
+
+            index = (index +1) % objs.Length;
+        }
+    }
+}
