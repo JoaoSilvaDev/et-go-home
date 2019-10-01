3efd0df6faf4d3dbf5b535a7402909cc99445dac
Joao Silva <joaosdev@gmail.com>
2019-10-01 16:51:04 +0100

    ipuabfiubasfou

---
+++ Assets/Scripts/AstronautWakeupScripting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AstronautWakeupScripting : MonoBehaviour
+{
+    public float minDistance;
+    public GameObject portalVFX;
+    private bool wokeup = false;
+
+    private Animator animator;
+    private Transform cam;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        cam = GameObject.FindObjectOfType<Camera>().transform;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, cam.position) < minDistance && !wokeup)
+        {
+            wokeup = true;
+
+            animator.SetBool("getup", true);
+            portalVFX.SetActive(true);
+
+            StartCoroutine(WaitBeforeNextScene());
+        }
+    }
+
+    IEnumerator WaitBeforeNextScene()
+    {
+        Debug.Log("Started wai");
+        yield return new WaitForSeconds(4);
+        Debug.Log("Loading Scene");
+        SceneManager.LoadScene("03");
+    }
+}
---
+++ Assets/Scripts/SwitchMaterialsOnTouch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMaterialsOnTouch : MonoBehaviour
+{
+    public Material alternativenMaterial;
+    private Material startingMaterial;
+        
+    private MeshRenderer meshRenderer;
+    private bool alternativeMat = false;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        startingMaterial = meshRenderer.material;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+            SwitchMaterial();
+    }
+
+    void SwitchMaterial()
+    {
+        alternativeMat = !alternativeMat;
+
+        if (alternativeMat)
+            meshRenderer.material = alternativenMaterial;
+        else
+            meshRenderer.material = startingMaterial;
+    }
+}
