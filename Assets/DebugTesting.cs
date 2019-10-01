c65db4cdb2cc84ec5da0c8b1f8d9b77312e9f2bf
Joao Silva <joaosdev@gmail.com>
2019-10-01 11:31:32 +0100

    started 3d occlusion with marker

---
+++ Assets/DebugTesting.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTesting : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("YEY");
+    }
+
+    void Update()
+    {
+        Debug.Log("YE");
+    }
+}
---
+++ Assets/Scripts/TrackedImageInfoManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARTrackedImageManager))]
+public class TrackedImageInfoManager : MonoBehaviour
+{
+    ARTrackedImageManager m_TrackedImageManager;
+
+    void Awake()
+    {
+        m_TrackedImageManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    void OnEnable()
+    {
+        m_TrackedImageManager.trackedImagesChanged += OnTrackedImagesChanged;
+    }
+
+    void OnDisable()
+    {
+        m_TrackedImageManager.trackedImagesChanged -= OnTrackedImagesChanged;
+    }
+
+    void OnTrackedImagesChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var trackedImage in eventArgs.added)
+        {
+            trackedImage.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+}
