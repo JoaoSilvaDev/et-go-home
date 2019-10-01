3b01b845e9c6758ea26bf52b761c84ba81825397
Roberto Jr Dev <junior.rmg.rj@gmail.com>
2019-10-01 18:12:21 +0100

    novo marker done

---
+++ Assets/Scripts/Tests Only/Tracker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+
+    [SerializeField] private GameObject prefab;
+
+    private GameObject _instance;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (!_instance)
+            {
+                _instance = Instantiate(prefab, transform.position, transform.rotation);
+            }
+            else
+            {
+                _instance.transform.position = transform.position;
+                _instance.transform.rotation = transform.rotation;
+            }
+        }
+    }
+
+}
